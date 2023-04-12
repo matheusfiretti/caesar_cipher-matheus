a0723b24be93d33dd0f9c00e49b6bc4672e8f6ff
Matheus Firetti <matheusfiretti@gmail.com>
2023-04-12 13:06:57 -0300

    final code version

---
+++ main.py
@@ -0,0 +1,54 @@
+print("""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+""")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+should_end = False
+
+while should_end is False:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    def encrypt(text, shift):
+        cipher = []
+        lista = list(text)
+        for i in lista:
+            indice = alphabet.index(i) + shift
+            cipher.append(alphabet[indice])
+            cipher_text = ''.join(cipher)
+        print(f'The encoded text is {cipher_text}')
+        
+        
+    def decrypt(text, shift):
+        cipher = []
+        lista = list(text)
+        for i in lista:
+            indice = alphabet.index(i) - shift
+            cipher.append(alphabet[indice])
+            cipher_text = ''.join(cipher)
+        print(f'The decoded text is {cipher_text}')
+    
+
+    if direction == 'encode':
+        encrypt(text, shift)
+    else:
+        decrypt(text, shift)
+        
+    restart = input('Type "yes" if you want to go again. Otherwise, type "no"\n')
+    if restart == 'no':
+        should_end = True
+        print('Goodbye')
